67de9966f5f7ee6854fefef69da0435d3be96522
tangmingheng <mhtang@thoughtworks.com>
2019-11-27 23:14:08 +0800

    创建页面结构

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 // 入口文件
-console.log('OK')
+import Vue from 'vue'
+import app from './App.vue'
+// 按需导入 Mint-UI 中的组件 
+import { Header } from 'mint-ui'
+// 导入 MUI 的样式
+import './lib/mui/css/mui.min.css'
+// 导入扩展图标样式
+import './lib/mui/css/icons-extra.css'
+
+// 1.1 导入路由的包
+import VueRouter from 'vue-router'
+// 1.2 安装路由
+Vue.use(VueRouter)
+// 1.3 导入自己的 router.js 路由模块
+import router from './router.js'
+
+Vue.component(Header.name, Header)
+
+var vm = new Vue({
+    el: "#app",
+    render: c => c(app),
+    router // 1.4 挂载路由对象到 VM 实例上
+})
