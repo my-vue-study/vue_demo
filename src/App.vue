d1e90a3feb984397bc652022380b9a63e052a00f
tangmingheng <mhtang@thoughtworks.com>
2019-11-28 22:34:58 +0800

    完成组件切换时候的动画效果

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <!-- 顶部 Header 区域 -->
     <mt-header fixed title="vue.demo"></mt-header>
     <!-- 中间的 路由 router-view 区域 -->
-    <router-view></router-view>
+    <transition>
+      <router-view></router-view>
+    </transition>
     <!-- 底部 Tabbar 区域 -->
     <nav class="mui-bar mui-bar-tab">
       <router-link class="mui-tab-item" to="/home">
@@ -36,5 +38,21 @@
 .app-container {
   padding-top: 40px;
   overflow-x: hidden;
+}
+
+.v-enter {
+  opacity: 0;
+  transform: translateX(100%);
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
 }
 </style>
