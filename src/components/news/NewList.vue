40c93aa477d5ab29ce7f516b1c13e4ff79b05bb3
tangmingheng <mhtang@thoughtworks.com>
2019-11-30 11:49:57 +0800

    绘制新闻资讯列表页面布局

---
+++ src/components/news/NewList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <ul class="mui-table-view">
+      <li class="mui-table-view-cell mui-media">
+        <img class="mui-media-object mui-pull-left" src="../../images/cbd.jpg" />
+        <div class="mui-media-body">
+          <h1>幸福</h1>
+          <p class="mui-ellipsis">
+            <span>发表时间：2012-12-12 12:12:12</span>
+            <span>点击：0次</span>
+          </p>
+        </div>
+      </li>
+      <li class="mui-table-view-cell mui-media">
+        <img class="mui-media-object mui-pull-left" src="../../images/muwu.jpg" />
+        <div class="mui-media-body">
+          <h1>幸福</h1>
+          <p class="mui-ellipsis">
+            <span>发表时间：2012-12-12 12:12:12</span>
+            <span>点击：0次</span>
+          </p>
+        </div>
+      </li>
+      <li class="mui-table-view-cell mui-media">
+        <img class="mui-media-object mui-pull-left" src="../../images/shuijiao.jpg" />
+        <div class="mui-media-body">
+          <h1>幸福</h1>
+          <p class="mui-ellipsis">
+            <span>发表时间：2012-12-12 12:12:12</span>
+            <span>点击：0次</span>
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.mui-table-view {
+  li {
+    h1 {
+      font-size: 14px;
+    }
+    .mui-ellipsis {
+      font-size: 12px;
+      color: #226aff;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShoppingCarContainer from './components/tabbar/ShoppingCarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
+import NewsList from './components/news/NewList.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
@@ -13,7 +14,8 @@ var router = new VueRouter({
     { path: '/home', component: HomeContainer },
     { path: '/member', component: MemberContainer },
     { path: '/shoppingCar', component: ShoppingCarContainer },
-    { path: '/search', component: SearchContainer }
+    { path: '/search', component: SearchContainer },
+    { path: '/home/newsList', component: NewsList }
   ],
   linkActiveClass: "mui-active" // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
 })
