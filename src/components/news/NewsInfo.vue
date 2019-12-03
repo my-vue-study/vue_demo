7feddc4f8a514088d0a2d4893c6909473890ae42
tangmingheng <mhtang@thoughtworks.com>
2019-12-03 14:45:18 +0800

    新闻详情页传参跳转实现

---
+++ src/components/news/NewsInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{id}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id // 将 URL 地址中传递过来的 Id值，挂载到 data上，方便以后调用
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -5,7 +5,8 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShoppingCarContainer from './components/tabbar/ShoppingCarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
-import NewsList from './components/news/NewList.vue'
+import NewsList from './components/news/NewsList.vue'
+import NewsInfo from './components/news/NewsInfo.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
@@ -15,7 +16,8 @@ var router = new VueRouter({
     { path: '/member', component: MemberContainer },
     { path: '/shoppingCar', component: ShoppingCarContainer },
     { path: '/search', component: SearchContainer },
-    { path: '/home/newsList', component: NewsList }
+    { path: '/home/newsList', component: NewsList },
+    { path: '/home/newsInfo:id', component: NewsInfo }
   ],
   linkActiveClass: "mui-active" // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
 })
